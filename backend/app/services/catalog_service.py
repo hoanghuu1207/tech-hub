93d39f6a3d8ca3f2af497af801ff8426feb73d52
am_hoanghh <khronal777@gmail.com>
2026-06-30 21:41:18 +0700

    add log

--- backend/app/services/catalog_service.py
+++ backend/app/services/catalog_service.py
@@ -33,6 +33,13 @@ from app.schemas.catalog import (
 )
 
 logger = logging.getLogger("catalog")
+logger.setLevel(logging.INFO)
+if not logger.handlers:
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
 
 
 class CatalogService:
